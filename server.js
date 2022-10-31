cd3851924e7c61e71d119e7cdc633866e1c42500
armanmikoyan <arman.mikoyan1@gmail.com>
2022-10-31 18:03:57 +0400

    conect to db

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+const Post = require('./models/post')
 
 app.use(express.static('styles'))
 
 
-const createPath = (page) => {
-	return path.resolve(__dirname, 'pages', `${page}.html`)
-}
+app.set('view engine', 'ejs')
 
+
+const createPath = (page) => {
+	return path.resolve(__dirname, 'ejs-pages', `${page}.ejs`)
+}
+ 
+const db = 'mongodb+srv://arman:Pass321@cluster0.a7eewrn.mongodb.net/?retryWrites=true&w=majority'
+mongoose
+.connect(db)
+.then(()=>{
+	console.log('conected to db');
+})
+.catch((err)=>{
+	console.log('error');
+})
 
 
 app.listen(3000,(err)=>{
@@ -16,37 +30,74 @@ app.listen(3000,(err)=>{
 })
 
 
+app.use(express.urlencoded({extended: false}))
+
 
 app.get('/',(req,res)=>{
-	res.sendFile(createPath('index'))
+	const title = 'Home'
+	res.render(createPath('index'),{title})
 })
 
 
 
 app.get('/posts/:id',(req,res)=>{
-	res.sendFile(createPath('post'))
+	const title = 'Post'
+	const post = {
+					id:1,
+					text:"loremLorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus excepturi dolores atque sit praesentium quam nisi dicta numquam libero! Unde rerum libero dicta similique sapiente nesciunt voluptate magni ducimus incidunt?",
+					title:'Post Title',
+					date: '05.08.2001',
+					author:'Arman'
+				}
+	
+	res.render(createPath('post'),{title,post})
 })
 
 
 app.get('/posts',(req,res)=>{
-	res.sendFile(createPath('posts'))
+	const title = 'Posts'
+	const posts = [
+		{
+			id:1,
+			text:"loremLorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus excepturi dolores atque sit praesentium quam nisi dicta numquam libero! Unde rerum libero dicta similique sapiente nesciunt voluptate magni ducimus incidunt?",
+			title:'Post Title',
+			date: '05.08.2001',
+			author:'Arman'
+		}
+	]
+	res.render(createPath('posts'),{title,posts})
 })
 
 
-app.get('/add-post',(req,res)=>{
-	res.sendFile(createPath('add-post'))
+app.get('/add-post', (req,res)=>{
+	const title = 'New Post'
+	res.render(createPath('add-post'),{title})
+})
+
+app.post('/add-post',(req,res)=>{
+	const {title,author,text} = req.body
+	const post = new Post({title,author,text}).save().then((result)=>{
+		res.send(result)
+	}).catch((err)=>{
+		console.log(err);
+	})
+	
+
 })
 
 app.get('/contacts',(req,res)=>{
-	res.sendFile(createPath('contacts'))
+	const title = 'Contacts'
+	const contacts = [
+		{name:'Gmail', link:'https://mail.google.com/mail/u/0/#inbox'},
+		{name:'Github', link:'https://github.com/armanmikoyan'},
+		{name:'Facebook', link:'https://www.facebook.com'}
+	]
+	res.render(createPath('contacts'),{contacts,title})
 	
 })
 
-
-
-
-app.use((req,res)=>{
-	
+app.use((req,res)=>{	
+	const title = 'Error 404'
 	res.status(404).
-	sendFile(createPath('error'))
+	render(createPath('error'),{title})
 })
